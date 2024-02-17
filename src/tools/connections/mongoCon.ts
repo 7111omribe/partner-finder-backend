d45d6b5ca86c8500f90b8d7f132c7542efba4cff
omri ben david <ebdu12@gmail.com>
2024-02-17 16:00:41 +0200

    added mongo interface class

---
+++ src/tools/connections/mongoCon.ts
@@ -0,0 +1,32 @@
+import { MongoClient, Db, ServerApiVersion } from "mongodb";
+
+export class MongoInterface {
+    private conn: Db;
+
+    constructor(dbName: string) {
+        const client = this.createClient();
+        client.connect();
+        this.conn = client.db(dbName)
+
+    }
+
+    public async aggregate(collectionName: string, query: Record<string, any>[]) {
+        const collection = this.conn.collection(collectionName);
+        const result = await collection.aggregate(query).toArray();
+        return result;
+    }
+
+    private createClient() {
+        const uri = "mongodb+srv://ebdu12:LiTfDEhq9W0ZeCw6@cluster0.frreqdt.mongodb.net/?retryWrites=true&w=majority";
+        const client = new MongoClient(uri, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        });
+        return client;
+    }
+
+
+}
